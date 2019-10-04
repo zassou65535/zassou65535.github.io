cfa8975fc5acf75198f90fdcec7985aceb7beeb6
zassou <nakamura.k.bv@m.titech.ac.jp>
2019-10-04 21:59:12 +0900

    色変更、メニューバー追加

---
+++ pages/component/Menu/MenuBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="menubar_entity">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "menubar",
+        props: {
+        },
+    };
+</script>
+
+<style scoped lang="scss">
+.menubar_entity{
+    position:fixed;
+    width:100%;
+    height:60px;
+    top:0px;
+    left:0px;
+    background:rgba(200,200,0,0.5);
+}
+</style>
