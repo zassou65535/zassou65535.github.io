8467239db88846d17949545e29d38e15e468f841
zassou <nakamura.k.bv@m.titech.ac.jp>
2019-10-05 19:10:15 +0900

    canvasで円を描画できる機能追加

--- pages/component/Top/Canvas.vue
+++ pages/component/Top/Canvas.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="canvas">
+    <div class="canvas_wrapper">
+        <canvas id="back_canvas"></canvas>
+        <canvas id="fore_canvas"></canvas>
     </div>
 </template>
 
@@ -8,11 +10,32 @@
         name: "Canvas",
         props: {
         },
+        created:function(){//on load
+            this.DrawBackGround();
+        },
+        methods:{
+            DrawBackGround:function(){
+                if (!process.client) {
+                  return;
+                }
+                var back_canv = document.querySelector("#back_canvas");
+                var context = back_canv.getContext("2d");
+                this.DrawCircle(context,100,100,10);
+                context.stroke();
+            },
+            DrawCircle:function(context,x,y,radius){
+                context.beginPath();//path reset
+                context.arc(x,y,radius,0,2*Math.PI,false);
+                context.fillstyle = "rgba(255,0,0,1)"
+                context.fill();
+                context.linewidth = 0;
+            },
+        }
     };
 </script>
 
 <style scoped lang="scss">
-.canvas{
+.canvas_wrapper{
     position:fixed;
     width:100%;
     height:100%;
