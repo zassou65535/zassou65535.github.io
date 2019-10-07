21af1fa283d552c1c6884ff5b5c6ba38fbb89996
zassou <nakamura.k.bv@m.titech.ac.jp>
2019-10-07 18:17:32 +0900

    no message

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,20 @@
 <template>
 	<div>
+		<PageTitle>ABOUT</PageTitle>
+		<MainBodyContent>
+		</MainBodyContent>
 	</div>
 </template>
 
 <script>
+import PageTitle from "./component/Versatile/PageTitle.vue"
+import MainBodyContent from "./component/Versatile/MainBodyContent.vue"
 export default {
     name: "about",
     layout:"pageLayout",
     components: {
+    	PageTitle,
+    	MainBodyContent,
     },
     props: {
     },
