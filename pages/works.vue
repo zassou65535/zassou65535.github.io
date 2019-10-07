664e6e72f577a690f45a5e3376b20cb7946718fe
zassou <nakamura.k.bv@m.titech.ac.jp>
2019-10-07 20:20:59 +0900

    worksにもコンポーネントを適用してみる

--- pages/works.vue
+++ pages/works.vue
@@ -2,6 +2,10 @@
 	<div>
 		<PageTitle>WORKS</PageTitle>
 		<MainBodyContent>
+            <WrapperWithTitle>
+                <template v-slot:title><p>Works</p></template>
+                <template v-slot:content><p></p></template>
+            </WrapperWithTitle>
 		</MainBodyContent>
 	</div>
 </template>
@@ -9,12 +13,14 @@
 <script>
 import PageTitle from "./component/Versatile/PageTitle.vue"
 import MainBodyContent from "./component/Versatile/MainBodyContent.vue"
+import WrapperWithTitle from "./component/Versatile/WrapperWithTitle.vue"
 export default {
     name: "works",
     layout:"pageLayout",
     components: {
     	PageTitle,
     	MainBodyContent,
+        WrapperWithTitle,
     },
     props: {
     },
