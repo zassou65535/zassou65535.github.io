bcc5bf7afd3c04c929cef4d35167e1798ec395d0
zassou <nakamura.k.bv@m.titech.ac.jp>
2019-10-09 20:49:58 +0900

    サイトタイトルの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,11 +13,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "zassous_atelier" || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "zassous_atelier" || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
